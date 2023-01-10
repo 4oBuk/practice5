3cecfedbcacafcb3abbc73d60575657178d54ef9
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-10 17:55:59 +0200

    create artists service implementation

--- src/main/java/com/chornobuk/practice5/controllers/ArtistsController.java
+++ src/main/java/com/chornobuk/practice5/controllers/ArtistsController.java
@@ -1,6 +1,7 @@
 package com.chornobuk.practice5.controllers;
 
 import com.chornobuk.practice5.entities.Artist;
+import com.chornobuk.practice5.services.ArtistsService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("artists")
 public class ArtistsController {
+
+    private final ArtistsService artistsService;
+
+    public ArtistsController(ArtistsService artistsService) {
+        this.artistsService = artistsService;
+    }
 
     @GetMapping
     public Artist getAllArtistById() {
---
+++ src/main/java/com/chornobuk/practice5/services/impl/ArtistsServiceImpl.java
@@ -0,0 +1,16 @@
+package com.chornobuk.practice5.services.impl;
+
+import com.chornobuk.practice5.entities.Artist;
+import com.chornobuk.practice5.services.ArtistsService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtistsServiceImpl implements ArtistsService {
+
+    @Override
+    public Iterable<Artist> getAllArtists() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
