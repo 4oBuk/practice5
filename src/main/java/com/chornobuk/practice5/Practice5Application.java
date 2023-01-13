1abe3d82685c1c89a6b7dc7bcb085f36eddc6a3d
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-13 21:17:55 +0200

    add todos for test classes and application

--- src/main/java/com/chornobuk/practice5/Practice5Application.java
+++ src/main/java/com/chornobuk/practice5/Practice5Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Practice5Application {
-
+	// TODO add error handlers for controllers
 	public static void main(String[] args) {
 		SpringApplication.run(Practice5Application.class, args);
 	}
