919a494a6c2677c5a8b6d446dbabf1500d0345c3
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-14 17:51:17 +0200

    add test for artists controller

--- src/test/java/com/chornobuk/practice5/controllers/ArtistsControllerTest.java
+++ src/test/java/com/chornobuk/practice5/controllers/ArtistsControllerTest.java
@@ -1,5 +1,36 @@
 package com.chornobuk.practice5.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.chornobuk.practice5.data.Artists;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ArtistsControllerTest {
-    // TODO test standartd behavior
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void getAllArtists() throws Exception{
+        String jsonArtists = objectMapper.writeValueAsString(Artists.getAllArtists());
+        String responseBody = this.mockMvc.perform(get("/artists"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn().getResponse().getContentAsString();
+        assertEquals(jsonArtists, responseBody);
+    }
 }
